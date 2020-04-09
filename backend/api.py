796be7b7467fb9be26e4f6715d84999d466081b3
Adel Hassan <sean.dashals@gmail.com>
2020-04-09 11:01:04 -0500

    Added pylint

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,12 @@
+"""
+Forms the API layer that relays information between the databse, scraper, and frontend
+"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Homepage for the API
+    """
     return 'Welcome to the GrocerEZ API!'
-
