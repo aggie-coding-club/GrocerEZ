b7293ab0568af400a36827c8034567b5e0e24311
adg516 <arjun.grover1601@gmail.com>
2020-05-14 18:25:36 -0500

    Editing Ryan's walmart scraper and connecting it to the database. Takes scraped data and inserts it into database

--- backend/scraper/scrape_walmart.py
+++ backend/scraper/scrape_walmart.py
@@ -1,14 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import psycopg2
+
+con = psycopg2.connect("dbname='ddjr78vh1cvcc4' user='xpcdulaqqancpa' host='ec2-34-200-72-77.compute-1.amazonaws.com' password='0616d8130a659a869dacb11768aaf0d8f87f741b19fc8bf46d3af34c78f5ca85'")
+cur = con.cursor()
 
 def parseLink(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
     product_title = soup.find(class_='prod-ProductTitle font-normal').get_text()
-    rating = soup.find(class_='ReviewsHeader-ratingPrefix font-bold').get_text()
-    dollars = soup.find('span', {'class': 'price-characteristic'}).get_text()
-    cents = soup.find('span', {'class': 'price-mantissa'}).get_text()
+
+    rating = soup.find(class_='ReviewsHeader-ratingPrefix font-bold')
+    if(rating != None):
+        rating = rating.text
+    else:
+        rating = "N/A"
+
+    dollars = soup.find('span', {'class': 'price-characteristic'})
+    if(dollars != None):
+        dollars = dollars.text
+    else:
+        dollars = "0"
+    
+    cents = soup.find('span', {'class': 'price-mantissa'})
+    if(cents != None):
+        cents = cents.text
+    else:
+        cents = "0"
+
     price = float(dollars + '.' + cents)
 
     #Return dictionary of product information
@@ -25,12 +45,39 @@ def getProducts(query):
         products.append(parseLink("https://walmart.com" + links.get('href')))
     #Return list of dictionaries
     return products
+
+    
 # Execution of Main Program
-query = "lettuce"
+query = "broccoli"
 products = getProducts(query)
+#print('\n', products, '\n')
+print(len(products))
+count = 0
+
+
+cur.execute("delete from \"Item\"")
+con.commit()
+
 
 for item in products:
-    print("Item Name: " + item['product-title'])
-    print("Price: " + item['price'])
-    print("Rating: " + item['rating'])
-    print("")
+    try:
+        #variables
+        name = item['product-title']
+        store = "Walmart"
+        pricee = float(item['price'])
+        ppu = 0.00
+        #end Variables
+
+        cur.execute('INSERT INTO "Item" (name, price, priceperunit, store) VALUES(%s, %s, %s, %s)', [name, pricee, ppu, store])
+        con.commit()
+
+        #print("Item Name: " + name)
+        #print("Price: " + pricee)
+        #print("Rating: " + item['rating'])
+        #print("")
+        count += 1
+    except Exception as e:
+        print(e)
+        continue
+
+print(count)
