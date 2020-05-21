92fb41d188a1a68442e9bea970d48e23a4e72f26
Ryan O'Malley <rd.omalley@comcast.net>
2020-05-21 11:03:27 -0500

    Optimized selenium scraper

--- backend/scraper/scrape_walmart.py
+++ backend/scraper/scrape_walmart.py
@@ -1,36 +1,88 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-def parseLink(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
 
-    product_title = soup.find(class_='prod-ProductTitle font-normal').get_text()
-    rating = soup.find(class_='ReviewsHeader-ratingPrefix font-bold').get_text()
-    dollars = soup.find('span', {'class': 'price-characteristic'}).get_text()
-    cents = soup.find('span', {'class': 'price-mantissa'}).get_text()
-    price = float(dollars + '.' + cents)
+ZIPCODE = '77841'
 
-    #Return dictionary of product information
-    product_details = { 'product-title' : product_title, 'price' : str(price), 'rating' : rating}
-    return product_details
+driver_path = r'C:/Users/Ryan/Desktop/Web Drivers/chromedriver.exe'
 
-def getProducts(query):
-    url = "http://www.walmart.com/search/?query=" + query
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    products = []
-    #Find links for each search result, and parse
-    for links in soup.find_all("a", class_='search-result-productimage gridview display-block'):
-        products.append(parseLink("https://walmart.com" + links.get('href')))
-    #Return list of dictionaries
-    return products
-# Execution of Main Program
-query = "lettuce"
-products = getProducts(query)
+class WalmartScraper:
+    zipSet = False
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(driver_path, options=chrome_options)
 
-for item in products:
-    print("Item Name: " + item['product-title'])
+    def getProduct(self, productName):
+        self.driver.get('https://www.walmart.com/search/?query=' + productName)
+        URLS = self.getProductUrls(productName)
+
+        productInformation = [] #List of dictionaries, containing product information
+
+        session = requests.Session()
+        session.put('https://www.walmart.com/account/api/location',
+                    data={'postalCode': ZIPCODE, 'clientName': 'Web-Header-NDToggleBar', 'persistLocation': 'true'})
+
+        for url in URLS:
+            productInformation.append(self.parseProductLink(url,session))
+
+        return productInformation
+
+    def getProductUrls(self, queryString):
+        self.driver.get('https://www.walmart.com/search/?query=' + queryString)
+        URLS = []
+        for link in self.driver.find_element_by_class_name('search-product-result').find_elements_by_tag_name('a'):
+            if (link.get_attribute('class') == 'search-result-productimage gridview display-block'):
+                URLS.append(link.get_attribute('href'))
+        return URLS
+
+    #Requests + selenium combo?
+    def parseProductLink(self, url, session):
+        print('Opening url: ' + url) #debugging
+        page = session.get(url, headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(page.text, 'lxml')
+        try:
+            product_title = soup.find(class_='prod-ProductTitle font-normal').get_text()
+        except:
+            product_title = 'ERROR: ' + url
+        try:
+            rating = soup.find(class_='ReviewsHeader-ratingPrefix font-bold').get_text()
+        except:
+            rating = 'ERROR: ' + url
+        dollar = soup.select_one(
+            '#price > div > span.hide-content.display-inline-block-m > span > span.price-group > span.price-characteristic')
+        cent = soup.select_one(
+            '#price > div > span.hide-content.display-inline-block-m > span > span.price-group > span.price-mantissa')
+
+        if (dollar != None and cent != None):
+            price = dollar.text + '.' + cent.text
+        else:
+            price = 'ERROR: ' + url
+
+        # Return dictionary of product information
+        information = {
+            'product_title': product_title,
+            'product_identifier': 'requests-version',
+            'price': price,
+            'ratings': rating,
+            'stock_status': 'requests-version'
+        }
+        return information
+
+    def destructor(self):
+        self.driver.close()
+
+scraper = WalmartScraper()
+apples = scraper.getProduct('apples')
+scraper.destructor()
+for item in apples:
+    print("Item Name: " + item['product_title'])
+    print("Product ID: " + item['product_identifier'])
     print("Price: " + item['price'])
-    print("Rating: " + item['rating'])
-    print("")
+    print("Rating: " + item['ratings'])
+    print("Stock Status: " + item['stock_status'])
+    print()
