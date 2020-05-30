9ef37f18eb185b84c4f70f9482a97e2dc4cb2791
Christopher Gallardo <47118180+Galla299802@users.noreply.github.com>
2020-05-29 23:34:56 -0500

    In progress Scraper
    Scraper currently finds item names and price just fine, but runs into issues with the rating. Apparently people have had issues locating li classes inside ul, so I'm working on finding a solution to that, because at the moment, this code cannot locate the stars classes to construct the ratings.

--- backend/scraper/scrape_heb.py
+++ backend/scraper/scrape_heb.py
@@ -6,24 +6,47 @@ def parseLink(url):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     ##find values of each attribute
-    product_title = soup.find('h1', {'class': 'pdp-product-name'}).get_text()
 
+    try:
+        title_section = soup.find('h1', {'itemprop': 'name'})
+        product_title = title_section.text
+    except:
+        product_title = " "
+        pass
+    try:
+        price = soup.find('div', {'class': 'sale2'})
+        priceString = ' '.join(price.text.split())
+    except:
+        priceString = " "
+        pass
+    ##Try looking at alt text
+    # #counting the amount of full/half stars and adding to obtain the rating of the product
+    rating = 0
+    fullstars = []
+    for stars in soup.find_all("li", {'class': 'star active'}):
+        fullstars.append(stars)
 
-    # rating = soup.find(class_='ReviewsHeader-ratingPrefix font-bold').get_text()
-    ##putting together string for price
-    price = soup.find('span', {'id': 'addToCartPrice'}).get_text()
-    #Return dictionary of product information
-    product_details = { 'product-title' : product_title, 'price' : str(price)}
+    halfs =[]  
+    for halfstars in soup.find_all("li", {'class': 'star active-50'}):
+        halfs.append(halfstars)
+
+    rating = rating+len(fullstars)
+    rating = rating+len(halfs)
+    ratingString = str(rating)
+
+    # #Return dictionary of product information
+    product_details = { 'product-title' : product_title, 'price' : priceString, 'rating' : ratingString}
     return product_details
 
 def getProducts(query):
     url = "https://www.heb.com/search/?q=" + query
     page = requests.get(url)
+    print(page)
     soup = BeautifulSoup(page.text, 'html.parser')
     products = []
     #Find links for each search result, and parse
-    for links in soup.find_all("a", class_='search-result-productimage gridview display-block'):
-        products.append(parseLink("https://www.heb.com" + links.get('href')))
+    for links in soup.find_all("li", {'class' : 'responsivegriditem product-grid-large-fifth product-grid-small-6'}):
+        products.append(parseLink("https://www.heb.com/" + links.a.get('href')))
     #Return list of dictionaries
     return products
 
@@ -34,4 +57,6 @@ products = getProducts(query)
 for item in products:
     print("Item Name: " + item['product-title'])
     print("Price: " + item['price'])
-    ##print("Rating: " + item['rating'])
+    print("Rating: " + item['rating'])
+    print ("")
+    print("")
