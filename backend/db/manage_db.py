26466dfbdcf4196eed1d3e346a53ffb2ce3ea5dc
Adel Hassan <sean.dashals@gmail.com>
2020-07-06 15:55:27 -0500

    DB username and psasword change based on config

--- backend/db/manage_db.py
+++ backend/db/manage_db.py
@@ -9,15 +9,29 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+from os import path
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # read in password from config file
-config = open('db_password.txt', 'r')
-db_password = config.readline().strip()
+pw_config = open(path.join(path.dirname(__file__), 'db_password'), 'r')
+dev_pw = pw_config.readline().strip()
+prod_pw = pw_config.readline().strip()
+pw_config.close()
 
-db_string = "postgres://xpcdulaqqancpa:"+ db_password +"@ec2-34-200-72-77.compute-1.amazonaws.com:5432/ddjr78vh1cvcc4"
+un_config = open(path.join(path.dirname(__file__), 'db_username'), 'r')
+dev_un = un_config.readline().strip()
+prod_un = un_config.readline().strip()
+
+# build appropriate db_string
+db_string = ''
+if app.config['ENV'] == 'development':
+  db_string = "postgres://{}:{}@localhost/grocerez".format(dev_un, dev_pw)
+else:
+  db_string = "postgres://{}:{}@ec2-34-200-72-77.compute-1.amazonaws.com:5432/ddjr78vh1cvcc4".format(prod_un, prod_pw)
+print(db_string)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
 
