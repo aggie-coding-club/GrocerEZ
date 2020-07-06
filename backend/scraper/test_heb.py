3fc4ed99f0ee4158c3de8390c6f2edb54b8237f9
Christopher Gallardo <47118180+Galla299802@users.noreply.github.com>
2020-07-06 14:17:39 -0500

    Update test_heb.py
    Updated test, but it is currently giving a unicode error. Designed to work with VicTrev's Final Scrape_heb.

--- backend/scraper/test_heb.py
+++ backend/scraper/test_heb.py
@@ -1,19 +1,20 @@
 
 import requests
-import Scraper
+import final_scrape_heb
 
 def test_searching_for_products_works(monkeypatch):
     # arrange
     class MockText:
-        text = open('heb_lettuce_search.html').read()
+        text = open('heb_lettuce_search_full.html').read()
     def mock_parse_link(_):
         return {'product-title' : 'Lettuce', 'price' : '1.22'}
 
     monkeypatch.setattr(requests, 'get', lambda _ : MockText())
-    monkeypatch.setattr(scrape_heb, 'parseLink', mock_parse_link)
+    monkeypatch.setattr(final_scrape_heb, 'parseLink', mock_parse_link)
 
     # act
-    products = scrape_heb.getProduct('apple')
+    scraper = final_scrape_heb.HEBScraper()
+    products = scraper.getProduct('apple')
 
     # assert
     assert len(products) == 60
